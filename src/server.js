8e62be28569822885e41efa0501bc38946246897
Novelier Webbelier <novelier.webbelier@gmail.com>
2022-02-02 21:00:02 +0900

    Installing Morgan

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
-
 const PORT = 4000;
+
+app.use(morgan("dev"));
 
 const handleListening = () => {
   console.log(`✅ Server listening on PORT http://localhost:${PORT} 🚀`);
 };
 
 app.listen(PORT, handleListening);
+
+export default app;
