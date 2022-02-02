5179a19baf34a37c5f1d3fb482baad1e98fec724
Novelier Webbelier <96553411+Novelier-Webbelier@users.noreply.github.com>
2022-02-02 22:47:03 +0900

    [Front-end] Configuring Pug

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ import userRotuer from "./routers/userRouter";
 const app = express();
 const PORT = 4000;
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(morgan("dev"));
 
 app.use("/", rootRouter);
