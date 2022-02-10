44372f0c5fb88033285abffbafa0b0b5e18bd085
Novelier Webbelier <96553411+Novelier-Webbelier@users.noreply.github.com>
2022-02-10 20:00:40 +0900

    [Back-end] Remove feature : Edit Topics :sweat_smile:

--- src/routers/topicRouter.js
+++ src/routers/topicRouter.js
@@ -2,8 +2,6 @@ import express from "express";
 import {
   getCreateNewTopic,
   psotCreateNewTopic,
-  getEditTopic,
-  postEditTopic,
   watchTopic,
 } from "../controllers/topicController";
 
@@ -11,6 +9,5 @@ const topicRouter = express.Router();
 
 topicRouter.route("/new").get(getCreateNewTopic).post(psotCreateNewTopic);
 topicRouter.route("/:topicname").get(watchTopic);
-topicRouter.route("/:topicname/edit").get(getEditTopic).post(postEditTopic);
 
 export default topicRouter;
