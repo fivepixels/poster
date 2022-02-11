e7110e16f2646e64fe768c7f9d76a6070ce2a88e
Novelier Webbelier <96553411+Novelier-Webbelier@users.noreply.github.com>
2022-02-11 16:20:22 +0900

    [Front-end] Add Style many :)

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -2,4 +2,7 @@ import express from "express";
 
 const apiRouter = express.Router();
 
+apiRouter.post("/star/add/:postername/:username");
+apiRouter.post("/star/cancel/:postername/:username");
+
 export default apiRouter;
