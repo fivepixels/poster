97f65f514c5b3314c0e2e652e4838ecd36ba7bc3
Novelier Webbelier <96553411+Novelier-Webbelier@users.noreply.github.com>
2022-02-13 19:11:39 +0900

    [Front-end] Error resolution : Webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
-
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const BASE_JS_PATH = "./src/client/js/";
 
 module.exports = {
+  target: "node",
+  resolve: {
+    alias: {
+      path: require.resolve("path-browserify"),
+    },
+  },
   entry: {
     main: `${BASE_JS_PATH}main.js`,
     link: `${BASE_JS_PATH}link.js`,
@@ -13,14 +19,15 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
     }),
+    new NodePolyfillPlugin(),
   ],
-  mode: "development",
-  watch: true,
   output: {
     filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
+  mode: "development",
+  watch: true,
   module: {
     rules: [
       {
